4296e2cefb158937dc446dba8fca06840ef91d45
Markus Mayr <markus.mayr@outlook.com>
2025-08-10 14:25:16 +0200

    Implement reorder buttons with icons

--- frontend/lib/Competences/Frontend/Common/Reorder.hs
+++ frontend/lib/Competences/Frontend/Common/Reorder.hs
@@ -12,10 +12,13 @@ where
 import Competences.Command (Command)
 import Competences.Document.Id (Id)
 import Competences.Document.Order (Orderable, Reorder (..), idL)
+import Competences.Frontend.Common.Translate qualified as C
 import Competences.Frontend.SyncDocument (SyncDocumentRef, modifySyncDocument)
+import Competences.Frontend.View.Button qualified as V
+import Competences.Frontend.View.Icon qualified as V
 import GHC.Generics (Generic)
 import Miso qualified as M
-import Optics.Core ((.~), (^.), (?~))
+import Optics.Core ((.~), (?~), (^.))
 
 newtype ReorderModel a = ReorderModel
   { reorderFrom :: Maybe (Id a)
@@ -58,8 +61,12 @@ issueReorderCommand
   :: forall a p
    . SyncDocumentRef -> MkReorderCommand a -> Reorder a -> M.Effect p (ReorderModel a) (ReorderAction a)
 issueReorderCommand r f to = do
-  M.get >>= mapM_ (\c -> do
-            M.io_ $ modifySyncDocument r c) . mkChangeOrderCommand
+  M.get
+    >>= mapM_
+      ( \c -> do
+          M.io_ $ modifySyncDocument r c
+      )
+      . mkChangeOrderCommand
   M.modify $ #reorderFrom .~ Nothing
   where
     mkChangeOrderCommand :: ReorderModel a -> Maybe Command
@@ -77,7 +84,27 @@ viewReorderItem m item =
           then [cancelButton]
           else [moveBeforeButton, moveAfterButton]
   where
-    moveButton = M.button_ [M.onClick $ ReorderFrom $ item ^. idL] [M.text "Move"]
-    cancelButton = M.button_ [M.onClick CancelReorder] [M.text "Cancel"]
-    moveBeforeButton = M.button_ [M.onClick $ ReorderBefore $ item ^. idL] [M.text "Move before"]
-    moveAfterButton = M.button_ [M.onClick $ ReorderAfter $ item ^. idL] [M.text "Move after"]
+    moveButton =
+      V.iconButton
+        [M.onClick $ ReorderFrom $ item ^. idL]
+        V.RegularButton
+        V.IcnReorder
+        (C.translate' C.LblMove)
+    cancelButton =
+      V.iconButton
+        [M.onClick CancelReorder]
+        V.RegularButton
+        V.IcnCancel
+        (C.translate' C.LblCancel)
+    moveBeforeButton =
+      V.iconButton
+        [M.onClick $ ReorderBefore $ item ^. idL]
+        V.RegularButton
+        V.IcnArrowUp
+        (C.translate' C.LblInsertBefore)
+    moveAfterButton =
+      V.iconButton
+        [M.onClick $ ReorderAfter $ item ^. idL]
+        V.RegularButton
+        V.IcnArrowDown
+        (C.translate' C.LblInsertAfter)
