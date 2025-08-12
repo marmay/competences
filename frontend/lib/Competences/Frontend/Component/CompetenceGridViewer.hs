cb65f7027c7a086930341c717eeeeb23499d3e88
Markus Mayr <markus.mayr@outlook.com>
2025-08-12 07:56:37 +0200

    WIP

---
+++ frontend/lib/Competences/Frontend/Component/CompetenceGridViewer.hs
@@ -0,0 +1,92 @@
+module Competences.Frontend.Component.CompetenceGridViewer
+  ( Model (..)
+  , Action (..)
+  , emptyModel
+  , update
+  , view
+  )
+where
+
+import Competences.Document
+  ( Competence (..)
+  , CompetenceGrid (..)
+  , Document (..)
+  , User
+  , UserId
+  , emptyDocument
+  , ordered
+  )
+import Competences.Document.Competence (CompetenceLevelId, Level (..))
+import Competences.Frontend.Common qualified as C
+import Competences.Frontend.SyncDocument (DocumentChange)
+import Competences.Frontend.View qualified as V
+import Data.Set qualified as Set
+import GHC.Generics (Generic)
+import Miso qualified as M
+import Optics.Core (ix, (%), (&), (?~))
+
+data Model = Model
+  { document :: !Document
+  , highlighted :: !(Set.Set CompetenceLevelId)
+  , selectedUser :: !(Maybe User)
+  }
+  deriving (Eq, Generic, Show)
+
+emptyModel :: Model
+emptyModel =
+  Model
+    { document = emptyDocument
+    , highlighted = Set.empty
+    , selectedUser = Nothing
+    }
+
+data Action
+  = SelectUser !UserId
+  | SetHighlighted ![CompetenceLevelId]
+  | ResourceDetailsOf ![CompetenceLevelId]
+  | EvidenceDetailsOf ![CompetenceLevelId]
+  | UpdateDocument !DocumentChange
+  deriving (Eq, Generic, Show)
+
+update :: Action -> M.Effect p Action Model
+update (SelectUser userId) = pure ()
+-- M.modify (\m -> m & selectedUser ?~ m & #document % #users % ix userId)
+update _ = pure ()
+
+data Column
+  = DescriptionColumn
+  | LevelDescriptionColumn !Level
+  deriving (Eq, Show)
+
+view :: Model -> M.View Model Action
+view m =
+  V.vBox_
+    (V.Expand V.Start)
+    (V.Expand V.Center)
+    V.SmallGap
+    [ title
+    , description
+    , competences
+    ]
+  where
+    title = V.title_ m.document.competenceGrid.title
+    description = V.text_ m.document.competenceGrid.description
+    competences =
+      V.viewTable $
+        V.defTable
+          { V.columns =
+              [ DescriptionColumn
+              , LevelDescriptionColumn BasicLevel
+              , LevelDescriptionColumn IntermediateLevel
+              , LevelDescriptionColumn AdvancedLevel
+              ]
+          , V.rows = ordered m.document.competences
+          , V.columnHeader = \case
+              DescriptionColumn -> C.translate' C.LblCompetenceDescription
+              LevelDescriptionColumn l -> C.translate' $ C.LblCompetenceLevelDescription l
+          , V.cellContents = \competence ->
+              let
+               in \case
+                    DescriptionColumn -> V.text_ competence.description
+                    LevelDescriptionColumn level -> V.text_ "..."
+          }
