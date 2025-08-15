6f66d41e1f3bc78daa2f853e61878268da2cdd69
Markus Mayr <markus.mayr@outlook.com>
2025-08-15 09:56:41 +0200

    Ported CompetenceEditor to be a proper component

--- frontend/lib/Competences/Frontend/Component/CompetenceEditor.hs
+++ frontend/lib/Competences/Frontend/Component/CompetenceEditor.hs
@@ -1,8 +1,7 @@
 module Competences.Frontend.Component.CompetenceEditor
   ( Model (..)
   , Action (..)
-  , update
-  , view
+  , competenceEditorComponent
   )
 where
 
@@ -12,12 +11,11 @@ import Competences.Frontend.Common (Label (..), translate')
 import Competences.Frontend.SyncDocument (SyncDocumentRef, modifySyncDocument)
 import Competences.Frontend.View (applyLabelButton, cancelLabelButton, modalDialog)
 import Competences.Frontend.View.Form qualified as V
-import Data.Either.Extra (maybeToEither)
+import Data.Aeson (ToJSON)
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import Miso qualified as M
-import Miso.String qualified as M
-import Optics.Core (Lens', at, (%), (.~), (^.))
+import Optics.Core (Lens', at, non, (%), (.~), (^.))
 
 newtype Model = Model
   { competence :: Competence
@@ -31,36 +29,46 @@ data Action
   | CancelEditing
   deriving (Eq, Show, Generic)
 
-update :: SyncDocumentRef -> Action -> M.Effect p Model Action
-update _ (ChangeDescription s) = do
-  M.modify (descriptionLens .~ M.fromMisoString s)
-update _ (ChangeLevelDescription l s) = do
-  M.modify (levelDescriptionLens l .~ toMaybe s)
-update r CompleteEditing = do
-  c <- M.gets (^. #competence)
-  M.io_ $ modifySyncDocument r $ AddCompetence c
-update _ _ = pure ()
+competenceEditorComponent
+  :: forall closeMessage parent
+   . (ToJSON closeMessage)
+  => closeMessage -> SyncDocumentRef -> Competence -> M.Component parent Model Action
+competenceEditorComponent closeMessage syncDocumentRef competence =
+  M.component model update view
+  where
+    model = Model competence
 
-view :: Model -> M.View m Action
-view m =
-  let descriptionField =
-        V.formField_
-          (translate' LblCompetenceDescription)
-          $ textField ChangeDescription (Right $ M.ms $ m ^. descriptionLens)
-      levelTextField level =
-        V.formField_ (translate' $ LblCompetenceLevelDescription level) $
-          textField
-            (ChangeLevelDescription level)
-            (maybeToEither (translate' $ LblCompetenceLevelPlaceholder level) (m ^. levelDescriptionLens level))
-   in modalDialog
-        []
-        [ V.form_
-            (translate' LblEditCompetence)
-            ([descriptionField] <> map levelTextField levels)
-            [ applyLabelButton [M.onClick CompleteEditing]
-            , cancelLabelButton [M.onClick CancelEditing]
+    update (ChangeDescription s) = M.modify (descriptionLens .~ M.fromMisoString s)
+    update (ChangeLevelDescription l s) = M.modify (levelDescriptionLens l .~ M.fromMisoString s)
+    update CompleteEditing = do
+      c <- M.gets (^. #competence)
+      M.io_ $ modifySyncDocument syncDocumentRef $ AddCompetence c
+      M.mailParent closeMessage
+    update CancelEditing = M.mailParent closeMessage
+
+    view m =
+      let descriptionField =
+            V.formField_
+              (translate' LblCompetenceDescription)
+              $ textField ChangeDescription (Right $ M.ms $ m ^. descriptionLens)
+          levelTextField level =
+            V.formField_ (translate' $ LblCompetenceLevelDescription level) $
+              textField
+                (ChangeLevelDescription level)
+                (withPlaceholder (translate' $ LblCompetenceLevelPlaceholder level) (m ^. levelDescriptionLens level))
+       in modalDialog
+            []
+            [ V.form_
+                (translate' LblEditCompetence)
+                ([descriptionField] <> map levelTextField levels)
+                [ applyLabelButton [M.onClick CompleteEditing]
+                , cancelLabelButton [M.onClick CancelEditing]
+                ]
             ]
-        ]
+
+withPlaceholder :: M.MisoString -> M.MisoString -> Either M.MisoString M.MisoString
+withPlaceholder placeholder "" = Left placeholder
+withPlaceholder _ s = Right s
 
 textField :: (M.MisoString -> Action) -> Either M.MisoString M.MisoString -> M.View m Action
 textField a (Left s) = textField' a (M.placeholder_ s)
@@ -69,12 +77,8 @@ textField a (Right s) = textField' a (M.value_ s)
 textField' :: (M.MisoString -> Action) -> M.Attribute Action -> M.View m Action
 textField' a attr = V.textarea_ [M.onInput a, attr]
 
-toMaybe :: (M.FromMisoString a) => M.MisoString -> Maybe a
-toMaybe "" = Nothing
-toMaybe s = Just $ M.fromMisoString s
-
 descriptionLens :: Lens' Model Text
 descriptionLens = #competence % #description
 
-levelDescriptionLens :: Level -> Lens' Model (Maybe Text)
-levelDescriptionLens l = #competence % #levelDescriptions % at l
+levelDescriptionLens :: Level -> Lens' Model Text
+levelDescriptionLens l = #competence % #levelDescriptions % at l % non ""
