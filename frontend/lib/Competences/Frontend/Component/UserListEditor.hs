aa6614aed1506fca6d361bb0d4c9873b2d7d6d94
Markus Mayr <markus.mayr@outlook.com>
2025-09-21 09:29:30 +0200

    Work on UserPage

--- frontend/lib/Competences/Frontend/Component/UserListEditor.hs
+++ frontend/lib/Competences/Frontend/Component/UserListEditor.hs
@@ -3,10 +3,11 @@ module Competences.Frontend.Component.UserListEditor
   )
 where
 
-import Competences.Command (Command)
+import Competences.Command (Command (..))
 import Competences.Common.IxSet qualified as Ix
-import Competences.Document (Document (..), User (..), UserId)
+import Competences.Document (ChangableField (..), Document (..), User (..), UserRole (..))
 import Competences.Frontend.Common qualified as C
+import Competences.Frontend.Component.Editable (editableComponent)
 import Competences.Frontend.SyncDocument
   ( DocumentChange (..)
   , SyncDocumentRef
@@ -14,20 +15,22 @@ import Competences.Frontend.SyncDocument
   , subscribeDocument
   )
 import Competences.Frontend.View qualified as V
-import Data.Map qualified as M
 import Data.Proxy (Proxy (..))
 import Data.Text (Text)
 import GHC.Generics (Generic)
 import Miso qualified as M
-import Optics.Core (ix, (%~), (&), (.~), (^.), (^?))
+import Optics.Core ((.~), (^.))
+import System.Random (randomIO)
 
 newtype Model = Model
   { users :: [User]
   }
   deriving (Eq, Generic, Show)
 
-newtype Action
+data Action
   = UpdateDocument DocumentChange
+  | IssueCommand Command
+  | NewUser
   deriving (Eq, Generic, Show)
 
 data UserListColumn
@@ -43,7 +46,13 @@ userListEditorComponent r u =
     model = Model []
 
     update :: Action -> M.Effect p Model Action
-    update (UpdateDocument (DocumentChange d _)) = M.modify $ #users .~ Ix.toAscList (Proxy @Text) (d ^. #users)
+    update (UpdateDocument (DocumentChange d _)) = do
+      M.io_ $ M.consoleLog $ "UserListEditor: UpdateDocument: " <> M.ms (show d)
+      M.modify $ #users .~ Ix.toAscList (Proxy @Text) (d ^. #users)
+    update (IssueCommand cmd) = M.io_ $ modifySyncDocument r cmd
+    update NewUser = M.io_ $ do
+      uid <- randomIO
+      modifySyncDocument r $ AddUser $ User {id = uid, name = "", role = Student}
 
     view :: Model -> M.View Model Action
     view m =
@@ -56,7 +65,7 @@ userListEditorComponent r u =
                     , RoleColumn
                     , DeleteColumn
                     ]
-                , rows = m ^. #users
+                , rows = m.users
                 , columnSpec = \case
                     DeleteColumn -> V.SingleActionColumn
                     _ -> V.AutoSizedColumn
@@ -65,23 +74,17 @@ userListEditorComponent r u =
                     RoleColumn -> C.translate' C.LblUserRole
                     DeleteColumn -> ""
                 , cellContents = \user -> \case
-                    NameColumn -> editableName user
-                    RoleColumn -> V.text_ "" -- editableRole (user ^. #id) (user ^. #role)
-                    DeleteColumn -> V.text_ "" -- deleteButton (user ^. #id)
+                    NameColumn -> editableName user.id
+                    RoleColumn -> editableRole user.id
+                    DeleteColumn -> deleteButton user.id
                 }
-          editableName = undefined
-       in -- editableName userId userName lockedBy
-          --   | u ^. #id == userId = V.text_ userName
-          --   | otherwise = case lockedBy of
-          --       NotLocked ->
-          --         V.hBox_
-          --           (V.Expand V.Start)
-          --           V.NoExpand
-          --           V.SmallGap
-          --           [ V.text_ userName
-          --           , V.spring_
-          --           , V.editButton [M.onClick $ IssueCommand (LockField (UserName userId))]
-          --           ]
-          --       LockedBy _ -> V.text_ (userName <> " (" <> C.translate' C.LblLocked <> ")")
-          --       LockedByUs t -> M.textInput_ userName [M.onInput $ UpdateName userId]
-          V.vBox_ V.NoExpand (V.Expand V.Start) V.SmallGap [title, users]
+          addButton =
+            V.hBox_
+              (V.Expand V.End)
+              V.NoExpand
+              V.NoGap
+              [V.iconLabelButton [M.key_ @M.MisoString "add-user", M.onClick NewUser] V.RegularButton V.IcnAdd (C.translate' C.LblAddUser)]
+          editableName u' = M.div_ [M.key_ $ M.ms (show (UserName u'))] M.+> editableComponent r u (UserName u')
+          editableRole u' = M.div_ [M.key_ $ M.ms (show (UserRole u'))] M.+> editableComponent r u (UserRole u')
+          deleteButton u' = V.deleteButton [M.onClick $ IssueCommand (RemoveUser u')]
+       in V.vBox_ V.NoExpand (V.Expand V.Start) V.SmallGap [title, users, addButton]
