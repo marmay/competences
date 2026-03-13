marmay/competences

commit 48fba679ff39d10995da9e129cd1c12f3b7a5ece

Convert delete buttons to hold-to-delete across generic Editor and MesoPlan
Replace single-click delete with 2-second hold-to-confirm for all entities
using the generic Editor framework (User, Evidence, Assignment, Task,
TaskGroup, SubTask, LessonNotes, Resource, Solution, CompetenceGrid,
Competence) and the standalone MesoPlan delete button.

frontend/lib/Competences/Frontend/Component/Editor/View.hs
@@ -17,6 +17,7 @@ import Competences.Document (User)
 import Competences.Frontend.Common qualified as C
 import Competences.Frontend.Component.Editor.Types (Action (..), Model, Reorder' (..))
 import Competences.Frontend.View.Button qualified as Button
+import Competences.Frontend.View.HoldButton qualified as HoldButton
 import Competences.Frontend.View.Icon qualified as Icon
 import Competences.Frontend.View.Layout qualified as Layout
 import GHC.Generics (Generic)
@@ -55,15 +56,16 @@ data EditorViewItem a patch f n = EditorViewItem
   , deleteState :: !DeleteState
   , moveState :: !MoveState
   , canApply :: !Bool
+  , holdDeleteState :: !(HoldButton.HoldState a)
   }
   deriving (Generic)
 
 type EditorView a patch f n = EditorViewData a patch f n -> M.View (Model a patch f) (Action a patch)
 
-compactButtons :: EditorViewItem a patch f n -> [M.View (Model a patch f) (Action a patch)]
+compactButtons :: (Eq a) => EditorViewItem a patch f n -> [M.View (Model a patch f) (Action a patch)]
 compactButtons = buttons Button.IconOnlyS
 
-extendedButtons :: EditorViewItem a patch f n -> [M.View (Model a patch f) (Action a patch)]
+extendedButtons :: (Eq a) => EditorViewItem a patch f n -> [M.View (Model a patch f) (Action a patch)]
 extendedButtons = buttons Button.IconTextS
 
 data ViewButtonStyle
@@ -71,7 +73,7 @@ data ViewButtonStyle
   | Extended
   deriving (Eq, Show)
 
-buttons :: Button.ButtonContentsStyle -> EditorViewItem a patch f n -> [M.View (Model a patch f) (Action a patch)]
+buttons :: (Eq a) => Button.ButtonContentsStyle -> EditorViewItem a patch f n -> [M.View (Model a patch f) (Action a patch)]
 buttons s item =
   case (item.editState, item.moveState, item.deleteState) of
     (_, MoveSource, _) ->
@@ -88,7 +90,7 @@ buttons s item =
     editButtons _ = []
     moveButtons NotMoving = [moveButton s a]
     moveButtons _ = []
-    deleteButtons Deletable = [deleteButton s a]
+    deleteButtons Deletable = [deleteButton item]
     deleteButtons _ = []
 
 -- | Render a row of buttons using appropriate layout
@@ -100,7 +102,6 @@ buttonRow _ btns = Button.buttonGroup btns
 editButton
   , finishEditButton
   , cancelEditButton
-  , deleteButton
   , moveButton
   , cancelMoveButton
   , moveBeforeButton
@@ -111,8 +112,10 @@ editButton
 editButton s a = Button.secondary (Button.button (s, Icon.IcnEdit, C.LblEdit) (StartEditing a :: Action a patch))
 finishEditButton s a = Button.primary (Button.button (s, Icon.IcnApply, C.LblApply) (FinishEditing a :: Action a patch))
 cancelEditButton s a = Button.destructive (Button.button (s, Icon.IcnCancel, C.LblCancel) (CancelEditing a :: Action a patch))
-deleteButton s a = Button.destructive (Button.button (s, Icon.IcnDelete, C.LblDelete) (Delete a :: Action a patch))
 moveButton s a = Button.secondary (Button.button (s, Icon.IcnReorder, C.LblMove) (StartMoving a :: Action a patch))
+
+deleteButton :: forall a patch f n. (Eq a) => EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
+deleteButton viewItem = HoldButton.holdButton HoldDelete viewItem.holdDeleteState viewItem.item
 cancelMoveButton s _ = Button.destructive (Button.button (s, Icon.IcnCancel, C.LblCancel) (CancelMoving :: Action a patch))
 moveBeforeButton s a = Button.secondary (Button.button (s, Icon.IcnArrowUp, C.LblInsertBefore) (FinishMoving (Before' a) :: Action a patch))
 moveAfterButton s a = Button.secondary (Button.button (s, Icon.IcnArrowDown, C.LblInsertAfter) (FinishMoving (After' a) :: Action a patch))

frontend/lib/Competences/Frontend/Component/Planning/DetailView.hs
@@ -11,7 +11,7 @@ import Competences.Document (Assignment (..), Document (..), Lesson (..))
 import Competences.Document.Assignment (AssignmentName (..))
 import Competences.Document.Lesson (ActionForm (..), LessonId, LessonPhase (..))
 import Competences.Document.LessonNotes (LessonNotes (..))
-import Competences.Document.MesoPlan (MesoPlan (..))
+import Competences.Document.MesoPlan (MesoPlan (..), MesoPlanId)
 import Competences.Document.Order (Reorder (..), orderMax, orderPosition)
 import Competences.Query.Lesson qualified as QLesson
 import Competences.Frontend.Common qualified as C
@@ -33,6 +33,7 @@ import Competences.Frontend.SyncContext
 import Competences.Frontend.View.Button qualified as Button
 import Competences.Frontend.View.DateDisplay qualified as DateDisplay
 import Competences.Frontend.View.Disclosure qualified as Disclosure
+import Competences.Frontend.View.HoldButton qualified as HoldButton
 import Competences.Frontend.View.Icon qualified as Icon
 import Competences.Frontend.View.Layout qualified as Layout
 import Competences.Frontend.View.Tailwind (class_)
@@ -57,6 +58,7 @@ data DetailModel = DetailModel
   , expandedPhases :: !(Set.Set LessonId)  -- Phases section per lesson
   , document :: !Document
   , reorderFrom :: !(Maybe LessonId)  -- Which lesson is being moved (reorder mode)
+  , holdDeleteMeso :: !(HoldButton.HoldState MesoPlanId)
   }
   deriving (Eq, Generic, Show)
 
@@ -72,6 +74,7 @@ data DetailAction
   | OpenMesoPlanEditorModal !MesoPlan
   | DeleteLesson !LessonId
   | DeleteMesoPlan
+  | HoldDeleteMeso !(HoldButton.HoldAction MesoPlanId)
   | PinLessonEvaluation !Lesson
   | PinAssignmentEvaluation !Assignment
   | OpenLessonImportModal
@@ -88,9 +91,10 @@ projectDetail
   -> Set.Set LessonId
   -> Set.Set LessonId
   -> Set.Set LessonId
+  -> HoldButton.HoldState MesoPlanId
   -> Document
   -> DetailModel
-projectDetail plan prevExpanded prevExpandedAssignments prevExpandedNotes prevExpandedPhases doc =
+projectDetail plan prevExpanded prevExpandedAssignments prevExpandedNotes prevExpandedPhases holdMeso doc =
   let -- Get fresh plan from document (may have been updated)
       plan' = maybe plan id $ Ix.getOne (doc.mesoPlans Ix.@= plan.id)
       lessons' = QLesson.mesoPlanLessons doc plan'.id
@@ -103,7 +107,7 @@ projectDetail plan prevExpanded prevExpandedAssignments prevExpandedNotes prevEx
       expandedAssignments' = Set.intersection prevExpandedAssignments lessonIds
       expandedNotes' = Set.intersection prevExpandedNotes lessonIds
       expandedPhases' = Set.intersection prevExpandedPhases lessonIds
-   in DetailModel plan' lessons' expanded expandedAssignments' expandedNotes' expandedPhases' doc Nothing
+   in DetailModel plan' lessons' expanded expandedAssignments' expandedNotes' expandedPhases' doc Nothing holdMeso
 
 -- | View for planning - allows editing meso plan and lessons
 detailView
@@ -121,7 +125,7 @@ detailComponent r initialPlan =
     { M.subs = [subscribeDocument r DocumentUpdated]
     }
   where
-    initialModel = DetailModel initialPlan [] Nothing Set.empty Set.empty Set.empty emptyDocument Nothing
+    initialModel = DetailModel initialPlan [] Nothing Set.empty Set.empty Set.empty emptyDocument Nothing HoldButton.emptyHoldState
 
     emptyDocument =
       Document
@@ -148,7 +152,7 @@ detailComponent r initialPlan =
         , draftAssignments = Ix.empty
         }
 
-    update (DocumentUpdated dc) = M.modify $ \m -> projectDetail m.mesoPlan m.expandedLessonId m.expandedAssignments m.expandedNotes m.expandedPhases dc.document
+    update (DocumentUpdated dc) = M.modify $ \m -> projectDetail m.mesoPlan m.expandedLessonId m.expandedAssignments m.expandedNotes m.expandedPhases m.holdDeleteMeso dc.document
 
     update CreateNewLesson = do
       m <- M.get
@@ -212,6 +216,11 @@ detailComponent r initialPlan =
       m <- M.get
       M.io_ $ modifySyncDocument r (MesoPlans $ OnMesoPlans $ Delete m.mesoPlan.id)
 
+    update (HoldDeleteMeso ha) =
+      HoldButton.handleHoldAction #holdDeleteMeso doDelete HoldDeleteMeso ha
+      where
+        doDelete mpId = modifySyncDocument r (MesoPlans $ OnMesoPlans $ Delete mpId)
+
     update (PinLessonEvaluation lesson) = do
       m <- M.get
       M.io_ $ pinLessonEvaluator r m.mesoPlan.dateFrom lesson
@@ -260,7 +269,7 @@ detailComponent r initialPlan =
                     ]
                 , Layout.hFlow Layout.gapT
                     [ Button.ghostSm (Button.button Icon.IcnEdit (OpenMesoPlanEditorModal m.mesoPlan))
-                    , Button.destructiveSm (Button.button Icon.IcnDelete DeleteMesoPlan)
+                    , HoldButton.holdButton HoldDeleteMeso m.holdDeleteMeso m.mesoPlan.id
                     ]
                 ]
             ]

frontend/lib/Competences/Frontend/Component/Editor.hs
@@ -31,6 +31,7 @@ import Competences.Frontend.Component.Editor.Editable
   , withModify
   , withReorder
   )
+import Competences.Frontend.View.HoldButton qualified as HoldButton
 import Competences.Frontend.Component.Editor.EditorField
 import Competences.Frontend.Component.Editor.Types (Action (..), Model (..))
 import Competences.Frontend.Component.MarkdownEditor (isContentValid)
@@ -87,7 +88,7 @@ editorComponent e r =
     { M.subs = [subscribeDocument r UpdateDocument]
     }
   where
-    model = Model Nothing Map.empty Nothing Nothing Map.empty Map.empty
+    model = Model Nothing Map.empty Nothing Nothing Map.empty Map.empty HoldButton.emptyHoldState
 
     runCommand :: Maybe Command -> IO ()
     runCommand Nothing = pure ()
@@ -121,6 +122,10 @@ editorComponent e r =
         withReorder e.editable d reorderFrom' reorderAction
       M.modify (#reorderFrom .~ Nothing)
     update (Delete a) = M.io_ $ runCommand $ withDelete e.editable a
+    update (HoldDelete ha) =
+      HoldButton.handleHoldAction #holdDeleteState doDelete HoldDelete ha
+      where
+        doDelete a = runCommand $ withDelete e.editable a
     update (UpdatePatch original patched) =
       M.modify $ #patches %~ Map.insert original patched
     update (UpdateDocument (DocumentChange newDocument _)) = do
@@ -128,7 +133,7 @@ editorComponent e r =
       M.io_ $ M.focus refocusTargetString
 
     updateModel :: Document -> Model a patch f -> Model a patch f
-    updateModel d (Model _ patches reorderFrom _ _ contentStates) =
+    updateModel d (Model _ patches reorderFrom _ _ contentStates holdDelete) =
       let es = e.editable.get d
           myEdits = filter (\(_, u) -> u == Just (syncDocumentEnv r ^. #connectedUser % #id)) (toList es)
           patches' = Map.fromList $ map (\(e', _) -> (e', fromMaybe def (Map.lookup e' patches))) myEdits
@@ -136,7 +141,7 @@ editorComponent e r =
           users = Map.fromList $ map (\u -> (u ^. #id, u)) (IxSet.toList $ d ^. #users)
           -- Retain content states only for entities still being edited
           contentStates' = Map.restrictKeys contentStates (Map.keysSet patches')
-       in Model (Just es) patches' reorderFrom refocusTarget users contentStates'
+       in Model (Just es) patches' reorderFrom refocusTarget users contentStates' holdDelete
 
     view :: Model a patch f -> M.View (Model a patch f) (Action a patch)
     view m = case m.entries of
@@ -175,4 +180,4 @@ editorComponent e r =
                 zipWith (\(n, f) t -> (n, f.editor t item item')) e.fields (refocusTarget : repeat False)
             | otherwise = fmap (\(n, f) -> (n, f.viewer item)) e.fields
           canApply = all isContentValid (Map.findWithDefault Map.empty item m.contentStates)
-       in EditorViewItem {item, editState, moveState, deleteState, fieldData, canApply}
+       in EditorViewItem {item, editState, moveState, deleteState, fieldData, canApply, holdDeleteState = m.holdDeleteState}

frontend/lib/Competences/Frontend/Component/Editor/FlowView.hs
@@ -8,7 +8,7 @@ import Competences.Frontend.View.Layout qualified as Layout
 import Data.Tuple (Solo (..))
 import Optics.Core ((^.))
 
-editorFlowView :: EditorView a patch Solo n
+editorFlowView :: (Eq a) => EditorView a patch Solo n
 editorFlowView viewData =
   let (MkSolo item) = viewData ^. #items
    in Layout.hFlow

frontend/lib/Competences/Frontend/Component/Editor/FormView.hs
@@ -17,21 +17,21 @@ import qualified Competences.Frontend.View as V
 import qualified Competences.Frontend.Common as C
 import qualified Competences.Frontend.View.Button as Button
 
-editorFormView :: Foldable f => M.View (Model a patch f) (Action a patch) -> M.MisoString -> (n -> M.MisoString) -> EditorView a patch f n
+editorFormView :: (Eq a, Foldable f) => M.View (Model a patch f) (Action a patch) -> M.MisoString -> (n -> M.MisoString) -> EditorView a patch f n
 editorFormView onMissing title toText viewData =
   let maybeFirst = foldr (\x _ -> Just x) Nothing viewData.items
   in maybe onMissing (showForm title toText) maybeFirst
 
-editorFormView' :: Foldable f => M.MisoString -> (n -> M.MisoString) -> EditorView a patch f n
+editorFormView' :: (Eq a, Foldable f) => M.MisoString -> (n -> M.MisoString) -> EditorView a patch f n
 editorFormView' = editorFormView onMissing
   where onMissing = V.centeredPlaceholder (C.translate' C.LblPleaseSelectItem)
 
-editorFormViewS :: M.MisoString -> (n -> M.MisoString) -> EditorView a patch Solo n
+editorFormViewS :: (Eq a) => M.MisoString -> (n -> M.MisoString) -> EditorView a patch Solo n
 editorFormViewS title toText viewData =
   let (MkSolo item) = viewData.items
    in showForm title toText item
 
-showForm :: M.MisoString -> (n -> M.MisoString) -> EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
+showForm :: (Eq a) => M.MisoString -> (n -> M.MisoString) -> EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
 showForm title toText item =
    V.form_
         title
@@ -40,12 +40,12 @@ showForm title toText item =
 
 -- | Inline editor form view - shows just the form fields without header/buttons
 -- Used for embedding editors in lists or other compact contexts
-editorFormViewInline :: Foldable f => (n -> M.MisoString) -> EditorView a patch f n
+editorFormViewInline :: (Eq a, Foldable f) => (n -> M.MisoString) -> EditorView a patch f n
 editorFormViewInline toText viewData =
   let maybeFirst = foldr (\x _ -> Just x) Nothing viewData.items
    in maybe V.empty (showFormInline toText) maybeFirst
 
-showFormInline :: (n -> M.MisoString) -> EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
+showFormInline :: (Eq a) => (n -> M.MisoString) -> EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
 showFormInline toText item =
   MH.div_
     [class_ "space-y-3"]

frontend/lib/Competences/Frontend/Component/Editor/TableView.hs
@@ -15,7 +15,7 @@ data TableRowEditorColumn n
   | TableRowEditorActionColumn
 
 editorTableRowView
-  :: (Foldable f) => (n -> Table.TableColumnSpec) -> Table.TableColumnSpec -> EditorView a patch f n
+  :: (Eq a, Foldable f) => (n -> Table.TableColumnSpec) -> Table.TableColumnSpec -> EditorView a patch f n
 editorTableRowView specOf actionSpec viewData =
   Table.viewTable $
     Table.Table
@@ -29,6 +29,6 @@ editorTableRowView specOf actionSpec viewData =
           Table.tableRow $ map snd r.fieldData <> [Layout.hFlow Layout.gapS $ compactButtons r]
       }
 
-editorTableRowView' :: (Foldable f) => EditorView a patch f M.MisoString
+editorTableRowView' :: (Eq a, Foldable f) => EditorView a patch f M.MisoString
 editorTableRowView' =
   editorTableRowView (Table.TableColumnSpec Table.AutoSizedColumn) (Table.TableColumnSpec Table.TripleActionColumn "")

frontend/lib/Competences/Frontend/Component/Editor/Types.hs
@@ -11,6 +11,7 @@ import Competences.Document.Id (Id)
 import Competences.Document.Order (Reorder (..))
 import Competences.Frontend.Component.MarkdownEditor (ContentState)
 import Competences.Frontend.SyncContext (DocumentChange)
+import Competences.Frontend.View.HoldButton (HoldAction, HoldState)
 import Competences.TaskContent.RichContent (RichContent)
 import Data.Foldable (toList)
 import Data.Map qualified as Map
@@ -24,6 +25,7 @@ data Model a patch f = Model
   , refocusTarget :: !(Maybe a)
   , users :: !(Map.Map UserId User)
   , contentStates :: !(Map.Map a (Map.Map Text (ContentState RichContent)))
+  , holdDeleteState :: !(HoldState a)
   }
   deriving (Generic)
 
@@ -33,6 +35,7 @@ instance (Eq a, Eq patch, Functor f, Foldable f) => Eq (Model a patch f) where
       && a.patches == b.patches
       && a.reorderFrom == b.reorderFrom
       && a.contentStates == b.contentStates
+      && a.holdDeleteState == b.holdDeleteState
 
 data Action a patch
   = StartEditing !a
@@ -42,6 +45,7 @@ data Action a patch
   | CancelMoving
   | FinishMoving !(Reorder' a)
   | Delete !a
+  | HoldDelete !(HoldAction a)
   | UpdatePatch !a !patch
   | UpdateDocument !DocumentChange
   deriving (Eq, Show)

commit c95ff95f6bac1227e7102ad939640bc8b83b311f

Fix hold-to-delete button size regression from commit 48fba679
The holdButton now accepts ButtonVariant, ButtonSize, and ButtonContents
instead of hardcoding btn-sm-icon-destructive. Editor delete buttons
restore their pre-refactoring sizes: regular icon+text in extended mode,
regular icon-only in compact mode.

frontend/lib/Competences/Frontend/Component/Editor/View.hs
@@ -90,7 +90,7 @@ buttons s item =
     editButtons _ = []
     moveButtons NotMoving = [moveButton s a]
     moveButtons _ = []
-    deleteButtons Deletable = [deleteButton item]
+    deleteButtons Deletable = [deleteButton s item]
     deleteButtons _ = []
 
 -- | Render a row of buttons using appropriate layout
@@ -114,8 +114,9 @@ finishEditButton s a = Button.primary (Button.button (s, Icon.IcnApply, C.LblApp
 cancelEditButton s a = Button.destructive (Button.button (s, Icon.IcnCancel, C.LblCancel) (CancelEditing a :: Action a patch))
 moveButton s a = Button.secondary (Button.button (s, Icon.IcnReorder, C.LblMove) (StartMoving a :: Action a patch))
 
-deleteButton :: forall a patch f n. (Eq a) => EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
-deleteButton viewItem = HoldButton.holdButton HoldDelete viewItem.holdDeleteState viewItem.item
+deleteButton :: forall a patch f n. (Eq a) => Button.ButtonContentsStyle -> EditorViewItem a patch f n -> M.View (Model a patch f) (Action a patch)
+deleteButton Button.IconTextS viewItem = HoldButton.holdDeleteButton HoldDelete viewItem.holdDeleteState viewItem.item
+deleteButton _ viewItem = HoldButton.holdDeleteButtonSm HoldDelete viewItem.holdDeleteState viewItem.item
 cancelMoveButton s _ = Button.destructive (Button.button (s, Icon.IcnCancel, C.LblCancel) (CancelMoving :: Action a patch))
 moveBeforeButton s a = Button.secondary (Button.button (s, Icon.IcnArrowUp, C.LblInsertBefore) (FinishMoving (Before' a) :: Action a patch))
 moveAfterButton s a = Button.secondary (Button.button (s, Icon.IcnArrowDown, C.LblInsertAfter) (FinishMoving (After' a) :: Action a patch))

frontend/lib/Competences/Frontend/Component/Planning/DetailView.hs
@@ -269,7 +269,7 @@ detailComponent r initialPlan =
                     ]
                 , Layout.hFlow Layout.gapT
                     [ Button.ghostSm (Button.button Icon.IcnEdit (OpenMesoPlanEditorModal m.mesoPlan))
-                    , HoldButton.holdButton HoldDeleteMeso m.holdDeleteMeso m.mesoPlan.id
+                    , HoldButton.holdDeleteButtonSm HoldDeleteMeso m.holdDeleteMeso m.mesoPlan.id
                     ]
                 ]
             ]

frontend/lib/Competences/Frontend/Component/Submission.hs
@@ -399,7 +399,7 @@ submissionModalComponent r assignmentId userId _wm =
     subCell holding s SubColActions =
       Layout.hFlow (Layout.gapS <> Layout.crossCenter)
         [ Button.ghostSm (Button.button Icon.IcnView (PeekSubmission s.id))
-        , HoldButton.holdButton OnHoldDelete holding s.id
+        , HoldButton.holdDeleteButtonSm OnHoldDelete holding s.id
         ]
 
     kindBadge (DigitalSubmission _) = Badge.primary (Badge.badgeText (C.translate' C.LblAbgegeben))

frontend/lib/Competences/Frontend/View/HoldButton.hs
@@ -11,21 +11,28 @@
 -- 1. Add @HoldAction id@ to your action type (wrapped in a constructor)
 -- 2. Add @HoldState id@ to your model (initialise with 'emptyHoldState')
 -- 3. Delegate in update: @HoldButton.handleHoldAction #holdField doDelete WrapCtor ha@
--- 4. Render: @HoldButton.holdButton WrapCtor holdState thisId@
+-- 4. Render: @HoldButton.holdDeleteButtonSm WrapCtor holdState thisId@
 module Competences.Frontend.View.HoldButton
   ( HoldAction (..)
   , HoldState (..)
   , emptyHoldState
   , handleHoldAction
   , holdButton
+  , holdDeleteButton
+  , holdDeleteButtonSm
   , isHoldingId
   )
 where
 
 import Competences.Frontend.Common.Translate qualified as C
+import Competences.Frontend.View.Button qualified as Button
 import Competences.Frontend.View.Icon qualified as Icon
+import Competences.Frontend.View.Layout qualified as Layout
 import Competences.Frontend.View.Tailwind (class_)
 import Control.Concurrent (threadDelay)
+import Data.Maybe (maybeToList)
+import Data.Text (Text)
+import Data.Text qualified as T
 import Miso qualified as M
 import Miso.CSS qualified as MC
 import Miso.Html qualified as MH
@@ -97,7 +104,7 @@ handleHoldAction holdLens onExecute _wrap (ExecuteHold eid gen) = do
 handleHoldAction holdLens _onExecute _wrap HideHint =
   M.modify $ \m -> m & holdLens .~ (m ^. holdLens){showHint = False}
 
--- | Render a hold-to-delete button (trash icon with 2s fill animation).
+-- | Render a hold-to-activate button with configurable appearance.
 --
 -- During the hold, a full-screen transparent overlay captures mouseup
 -- anywhere on the page, so dragging outside the button and releasing
@@ -110,8 +117,11 @@ holdButton
   => (HoldAction id -> action)
   -> HoldState id
   -> id
+  -> Button.ButtonVariant
+  -> Button.ButtonSize
+  -> Button.ButtonContents
   -> M.View m action
-holdButton wrap hs eid =
+holdButton wrap hs eid variant size contents =
   let isHolding = isHoldingId hs eid
    in MH.div_
         [class_ "relative inline-flex"]
@@ -147,8 +157,8 @@ holdButton wrap hs eid =
             ]
             [ -- Button
               MH.button_
-                [class_ "btn-sm-icon-destructive"]
-                [Icon.iconS Icon.Small Icon.IcnDelete]
+                [class_ $ btnClass variant size contents]
+                [renderBtnContents size contents]
             , -- Progress fill overlay
               MH.div_
                 [ class_ $ "absolute inset-0 bg-white/30 rounded-md pointer-events-none"
@@ -161,3 +171,62 @@ holdButton wrap hs eid =
                 []
             ]
         ]
+
+-- | Small icon-only destructive hold button (for compact contexts: tables, flow views).
+holdDeleteButtonSm
+  :: (Eq id)
+  => (HoldAction id -> action)
+  -> HoldState id
+  -> id
+  -> M.View m action
+holdDeleteButtonSm wrap hs eid =
+  holdButton wrap hs eid Button.Destructive Button.Small (Button.IconOnly Icon.IcnDelete)
+
+-- | Regular destructive hold button with icon + "Delete" text (for form views).
+holdDeleteButton
+  :: (Eq id)
+  => (HoldAction id -> action)
+  -> HoldState id
+  -> id
+  -> M.View m action
+holdDeleteButton wrap hs eid =
+  holdButton wrap hs eid Button.Destructive Button.Regular
+    (Button.IconText Icon.IcnDelete (C.translate' C.LblDelete))
+
+-- | Build the Basecoat CSS class: @btn[-size][-icon][-variant]@
+btnClass :: Button.ButtonVariant -> Button.ButtonSize -> Button.ButtonContents -> Text
+btnClass v s c =
+  T.intercalate "-" $
+    ["btn"]
+      <> maybeToList (sizeClass s)
+      <> maybeToList (iconClass c)
+      <> maybeToList (variantClass v)
+  where
+    variantClass Button.Primary = Nothing
+    variantClass Button.Secondary = Just "secondary"
+    variantClass Button.Destructive = Just "destructive"
+    variantClass Button.Ghost = Just "ghost"
+    variantClass Button.Link = Just "link"
+    variantClass Button.Outline = Just "outline"
+
+    sizeClass Button.Small = Just "sm"
+    sizeClass Button.Regular = Nothing
+    sizeClass Button.Large = Just "lg"
+
+    iconClass (Button.IconOnly _) = Just "icon"
+    iconClass (Button.SizedIcon _ _) = Just "icon"
+    iconClass _ = Nothing
+
+renderBtnContents :: Button.ButtonSize -> Button.ButtonContents -> M.View m a
+renderBtnContents _s (Button.TextOnly t) = M.text t
+renderBtnContents s (Button.IconOnly i) = Icon.iconS (toIconSize s) i
+renderBtnContents _s (Button.SizedIcon sz i) = Icon.iconS sz i
+renderBtnContents s (Button.IconText i t) =
+  Layout.hFlow
+    (Layout.gapS <> Layout.hFull <> Layout.crossCenter)
+    [Icon.iconS (toIconSize s) i, MH.span_ [] [M.text t]]
+
+toIconSize :: Button.ButtonSize -> Icon.Size
+toIconSize Button.Small = Icon.Small
+toIconSize Button.Regular = Icon.Regular
+toIconSize Button.Large = Icon.Large