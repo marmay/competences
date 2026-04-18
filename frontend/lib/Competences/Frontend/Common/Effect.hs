22f182db5ca76821f9053f387cf52e8ec0c8e6db
Markus Mayr <markus.mayr@outlook.com>
2026-04-18 16:33:20 +0200

    Add FragmentDef, EmbeddedFragment, embedFragment, and toComponent
    FragmentDef bundles a widget's state, update, view, and subscriptions.
    embedFragment wires it into a parent via lens + action lifter, returning
    an EmbeddedFragment with ready-to-use fields. toComponent wraps a
    FragmentDef as a standalone Miso component. mapSub added for lifting
    subscription actions.

--- frontend/lib/Competences/Frontend/Common/Effect.hs
+++ frontend/lib/Competences/Frontend/Common/Effect.hs
@@ -2,6 +2,11 @@ module Competences.Frontend.Common.Effect
   ( GEffect
   , liftEffect
   , liftEffect_
+  , FragmentDef (..)
+  , EmbeddedFragment (..)
+  , embedFragment
+  , toComponent
+  , mapSub
   )
 where
 
@@ -24,10 +29,46 @@ liftEffect modelLens liftAction eff = rws $ \r s ->
     liftSchedule :: M.Schedule action' -> M.Schedule action
     liftSchedule (M.Schedule s sinkEff) = M.Schedule s $ \a -> sinkEff (a . liftAction)
 
--- | Like 'liftEffect' but discards the return value.
 liftEffect_
   :: Lens' model model'
   -> (action' -> action)
   -> GEffect parent model' action' a
   -> GEffect parent model action ()
 liftEffect_ modelLens liftAction eff = () <$ liftEffect modelLens liftAction eff
+
+mapSub :: (a -> b) -> M.Sub a -> M.Sub b
+mapSub f sub = \sink -> sub (sink . f)
+
+data FragmentDef parent model action view = FragmentDef
+  { initialModel :: model
+  , update :: action -> GEffect parent model action ()
+  , view :: model -> view
+  , subs :: [M.Sub action]
+  }
+
+data EmbeddedFragment parent parentModel parentAction fragmentModel fragmentAction view = EmbeddedFragment
+  { initialModel :: fragmentModel
+  , update :: fragmentAction -> GEffect parent parentModel parentAction ()
+  , view :: parentModel -> view
+  , subscribe :: [M.Sub parentAction]
+  }
+
+embedFragment
+  :: Lens' parentModel fragmentModel
+  -> (fragmentAction -> parentAction)
+  -> FragmentDef parent fragmentModel fragmentAction view
+  -> EmbeddedFragment parent parentModel parentAction fragmentModel fragmentAction view
+embedFragment lens lift frag = EmbeddedFragment
+  { initialModel = frag.initialModel
+  , update = \a -> liftEffect_ lens lift (frag.update a)
+  , view = \m -> frag.view (m ^. lens)
+  , subscribe = map (mapSub lift) frag.subs
+  }
+
+toComponent
+  :: FragmentDef p model action (model -> M.View model action)
+  -> M.Component p model action
+toComponent frag =
+  (M.component frag.initialModel frag.update (\m -> frag.view m m))
+    { M.subs = frag.subs
+    }
